5db1848f6b572c6e0c257f00212e89ab87e1aea0
yinchuandong <johncdyin@gmail.com>
2018-08-22 14:19:20 +0800

    update:

--- data_query.py
+++ data_query.py
@@ -2,18 +2,18 @@ from __future__ import print_function
 from db_util import Symptom, RelatedSymptom, Disease, RelatedDisease
 
 
-def select_related_symtoms(symptom_id):
-    query_related_symtoms = Symptom\
+def select_related_symptoms(symptom_id):
+    query_related_symptoms = Symptom\
         .select(Symptom, RelatedSymptom.rank) \
         .join(RelatedSymptom, on=(RelatedSymptom.related_symptom_id == Symptom.id)) \
         .where(RelatedSymptom.main_symptom_id == symptom_id) \
         .order_by(RelatedSymptom.rank.desc())
 
-    related_symtoms = []
-    for r in query_related_symtoms:
-        # related_symtoms.append((r.name, r.related_symptom[0].rank))
-        related_symtoms.append(r.name)
-    return related_symtoms
+    related_symptoms = []
+    for r in query_related_symptoms:
+        # related_symptoms.append((r.name, r.related_symptom[0].rank))
+        related_symptoms.append(r.name)
+    return related_symptoms
 
 
 def select_related_diseases(symptom_id):
@@ -32,14 +32,14 @@ def select(symptom_names=['Dizziness']):
     query_symptom_ids = Symptom.select().where(Symptom.name << symptom_names)
     symptom_ids = [s.id for s in query_symptom_ids]
 
-    all_related_symtoms = []
+    all_related_symptoms = []
     for symptom_id in symptom_ids:
-        related_symtoms = select_related_symtoms(symptom_id)
-        all_related_symtoms.append(related_symtoms)
+        related_symptoms = select_related_symptoms(symptom_id)
+        all_related_symptoms.append(related_symptoms)
 
-    # get the intersection of given symtoms
-    ret_symptoms = set(all_related_symtoms[0])
-    for s in all_related_symtoms[1:]:
+    # get the intersection of given symptoms
+    ret_symptoms = set(all_related_symptoms[0])
+    for s in all_related_symptoms[1:]:
         ret_symptoms = ret_symptoms.intersection(s)
 
     all_related_diseases = []
@@ -58,7 +58,11 @@ def main():
     symptom_names = ['Dizziness', 'Shortness of Breath']
     # symptom_names = ['Dizziness']
     ret = select(symptom_names)
-    print(len(ret[0]), len(ret[1]))
+    print('related_symptoms:', len(ret[0]))
+    print(ret[0])
+    print('\n----------------------------\n')
+    print('related_diseases:', len(ret[1]))
+    print(ret[1])
     return
 
 
